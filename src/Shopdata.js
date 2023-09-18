6cadc16465e1894467f1d9744ff2365a38ca9b14
Apurva Prakash <mailapurva007@gmail.com>
2023-09-19 02:03:23 +0530

    Changed site data

--- src/Shopdata.js
+++ src/Shopdata.js
@@ -1,46 +1,207 @@
-const data = [
-    {
-      id: 1,
-      name: "Notebook",
-      price: 70,
-      color: "blue",
-      images: ['https://m.media-amazon.com/images/I/612LwAwHefL._SX679_.jpg','https://m.media-amazon.com/images/I/612LwAwHefL._SX679_.jpg','https://m.media-amazon.com/images/I/612LwAwHefL._SX679_.jpg','https://m.media-amazon.com/images/I/612LwAwHefL._SX679_.jpg'],
-      size: "King",
-      desc:"Good",
-      photo: "https://m.media-amazon.com/images/I/612LwAwHefL._SX679_.jpg"
-    },
-    {
-      id: 2,
-      name: "Samsung S22 ultra",
-      price: 92599,
-      color: "black",
-      size: "6.5 inches",
-      images:['https://m.media-amazon.com/images/I/61SvitSvKfL._SX679_.jpg','https://m.media-amazon.com/images/I/61SvitSvKfL._SX679_.jpg','https://m.media-amazon.com/images/I/61SvitSvKfL._SX679_.jpg'],
-      desc:"Good",
-      photo: "https://m.media-amazon.com/images/I/61SvitSvKfL._SX679_.jpg"
-    },
-    {
-      id: 3,
-      name: "VW Tv",
-      price: 8000,
-      color: "HD",
-      size: "32 inches",
-      images:['https://m.media-amazon.com/images/I/418GxB04szL._SY300_SX300_QL70_FMwebp_.jpg','https://m.media-amazon.com/images/I/418GxB04szL._SY300_SX300_QL70_FMwebp_.jpg'],
-      desc:"Discover the ancient secret to better health with our Premium Copper Water Bottle. Crafted with precision and care, this bottle is more than just a container; it's a wellness companion.",
-      photo:
-        "https://m.media-amazon.com/images/I/418GxB04szL._SY300_SX300_QL70_FMwebp_.jpg"
-    },
-    {
-      id: 4,
-      name: "Airpods Pro",
-      price: 12570,
-      color: "white",
-      size: "small",
-      images:['https://m.media-amazon.com/images/I/31Ri-FAMBUL._SY445_SX342_QL70_FMwebp_.jpg','https://m.media-amazon.com/images/I/31Ri-FAMBUL._SY445_SX342_QL70_FMwebp_.jpg','https://m.media-amazon.com/images/I/31Ri-FAMBUL._SY445_SX342_QL70_FMwebp_.jpg','https://m.media-amazon.com/images/I/31Ri-FAMBUL._SY445_SX342_QL70_FMwebp_.jpg'],
-      desc:"Good",
-      photo:
-        "https://m.media-amazon.com/images/I/31Ri-FAMBUL._SY445_SX342_QL70_FMwebp_.jpg"
-    }
-  ];
-  export default data;
+const additionalCopperBottleData = [
+  {
+      id: 5,
+      name: "Copper Bottle XL",
+      price: 750,
+      color: "copper",
+      images: ['https://nypost.com/wp-content/uploads/sites/2/2017/08/copper1a.jpg?quality=90&strip=all',
+               'https://n3.sdlcdn.com/imgs/i/z/i/KC-Brown-1000-ml-Copper-SDL170317004-1-37835.jpg',
+               'https://n3.sdlcdn.com/imgs/i/e/3/New-raj-enterprises-Plain-copper-SDL159455825-2-2b390.jpg'],
+      size: "750ml",
+      desc: "Experience the goodness of copper with our Copper Bottle XL. This large 750ml bottle is perfect for staying hydrated throughout the day while reaping the benefits of copper's health properties.",
+      photo: "https://www.copperhealer.com/wp-content/uploads/2020/04/pure-copper-water-bottle-hammered2.jpg"
+  },
+  {
+      id: 6,
+      name: "Copper Water Jug",
+      price: 1200,
+      color: "copper",
+      images: ['https://images-na.ssl-images-amazon.com/images/I/51ugMF2BaxL._SY879_.jpg',
+               'https://tse3.mm.bing.net/th?id=OIP.xmpRQVWlIpH7YHVGrWWf8gHaHg&pid=Api&P=0&h=180',
+               'https://i.etsystatic.com/23992766/r/il/2a6324/2555961706/il_1588xN.2555961706_3zt1.jpg'],
+      size: "1.5 liters",
+      desc: "Upgrade your hydration routine with our Copper Water Jug. With a generous 1.5-liter capacity, this jug is perfect for storing and serving copper-infused water, promoting better health.",
+      photo: "https://www.copperhealer.com/wp-content/uploads/2020/04/pure-copper-water-bottle-hammered2.jpg"
+  },
+  {
+      id: 7,
+      name: "Copper Travel Flask",
+      price: 600,
+      color: "copper",
+      images: ['https://n3.sdlcdn.com/imgs/i/e/l/Copper-Bottle-Plain-copper-bottle-SDL894097581-4-df94c.jpg',
+                'https://i.etsystatic.com/24234419/r/il/720b24/3125222521/il_1588xN.3125222521_6jwa.jpg',
+                 'https://images-na.ssl-images-amazon.com/images/I/61G1%2B9lziuL._SL1454_.jpg'],
+      size: "350ml",
+      desc: "Stay refreshed on the go with our Copper Travel Flask. This compact 350ml flask is designed for portability, allowing you to enjoy the benefits of copper wherever you are.",
+      photo: "https://www.copperhealer.com/wp-content/uploads/2020/04/pure-copper-water-bottle-hammered2.jpg"
+  },
+  {
+      id: 8,
+      name: "Copper Water Bottle Set",
+      price: 1500,
+      color: "copper",
+      images: ['https://i.pinimg.com/originals/3b/fe/3f/3bfe3f3a74ab5a0970408d9eedb459c3.jpg',
+               'https://img2.exportersindia.com/product_images/bc-full/2019/6/5472075/copper-hammered-plain-bottle-1561378584-4968342.jpeg',
+                'https://n1.sdlcdn.com/imgs/j/c/8/TAMRANSH-Printed-Copper-Bottle-Multicolour-SDL109758523-1-c308e.jpg'],
+      size: "500ml (Set of 2)",
+      desc: "Share the gift of health with our Copper Water Bottle Set. This set includes two 500ml copper bottles, making it perfect for you and a loved one to enjoy the benefits of copper-infused water together.",
+      photo: "https://www.copperhealer.com/wp-content/uploads/2020/04/pure-copper-water-bottle-hammered2.jpg"
+  },
+  {
+      id: 9,
+      name: "Copper Yoga Bottle",
+      price: 900,
+      color: "copper",
+      images: ['https://5.imimg.com/data5/SELLER/Default/2021/6/PZ/HG/CW/12253973/digital-print-copper-bottle.jpg',
+                'https://5.imimg.com/data5/TU/UB/ZC/SELLER-14424623/5-5-.JPG',
+                'https://i.etsystatic.com/24456550/r/il/1e8980/2494780167/il_1588xN.2494780167_9h7f.jpg'],
+      size: "800ml",
+      desc: "Elevate your yoga practice with our Copper Yoga Bottle. With a generous 800ml capacity, this bottle ensures you stay hydrated and refreshed during your yoga sessions, all while benefiting from the goodness of copper.",
+      photo: "https://www.copperhealer.com/wp-content/uploads/2020/04/pure-copper-water-bottle-hammered2.jpg"
+  },
+
   
+      {
+          id: 10,
+          name: "Copper Infusion Bottle",
+          price: 850,
+          color: "copper",
+          images: [
+              'https://5.imimg.com/data5/SELLER/Default/2021/6/EO/EL/JZ/12253973/digital-print-copper-bottle.jpg',
+              'https://cdn.notonthehighstreet.com/fs/99/b7/e51c-949c-4932-bb73-1bb36ec79582/original_pure-copper-water-bottle.jpg',
+              'https://5.imimg.com/data5/LN/AW/MY-8325355/pure-copper-bottle.jpg'
+          ],
+          size: "600ml",
+          desc: "Elevate your hydration routine with our Copper Infusion Bottle. This 600ml bottle allows you to infuse your water with fruits or herbs while enjoying the benefits of copper.",
+          photo: "https://www.copperhealer.com/wp-content/uploads/2020/04/pure-copper-water-bottle-hammered2.jpg"
+      },
+      {
+          id: 11,
+          name: "Copper Ayurvedic Bottle",
+          price: 950,
+          color: "copper",
+          images: [
+              'https://n3.sdlcdn.com/imgs/b/v/9/Tiwaritraders-Golden-Copper-Water-Bottle-SDL442032275-1-d247e.jpg',
+              'https://vrikshahomeware.com/wp-content/uploads/2020/01/ArtEngraved_Copper_Bottle_Angle2.jpg',
+              'https://www.battlane.com/uploads/product-images/2020/02/05/89/large/pure-copper-bottle-1-litre-pack-of-2-Copper-1.jpg'
+          ],
+          size: "750ml",
+          desc: "Experience the ancient wisdom of Ayurveda with our Copper Ayurvedic Bottle. This 750ml bottle is designed to store and dispense copper-infused water, promoting holistic well-being.",
+          photo: "https://www.copperhealer.com/wp-content/uploads/2020/04/pure-copper-water-bottle-hammered2.jpg"
+      },
+      {
+          id: 12,
+          name: "Copper Wellness Elixir",
+          price: 1200,
+          color: "copper",
+          images: [
+              'https://www.giftsmoss.com/wp-content/uploads/2018/08/Copper-Bottle-600ml-Plain.jpg',
+              'https://5.imimg.com/data5/SELLER/Default/2021/6/KQ/TG/CW/12253973/digital-print-copper-bottle.jpg',
+              'https://images.naptol.com/usr/local/csp/staticContent/product_images/horizontal/750x750/1-Litre-deisgner-copper-bottle-with-Brush-02.jpg'
+          ],
+          size: "1 liter",
+          desc: "Nourish your body with our Copper Wellness Elixir. This 1-liter bottle is ideal for storing and sipping copper-infused elixirs, promoting overall health and vitality.",
+          photo: "https://www.copperhealer.com/wp-content/uploads/2020/04/pure-copper-water-bottle-hammered2.jpg"
+      },
+      {
+          id: 13,
+          name: "Copper Love Bottle",
+          price: 750,
+          color: "copper",
+          images: [
+              'https://cdn.exoticindia.com/sculptures/zbq04.jpg',
+              'https://etbykayal.com/wp-content/uploads/2018/10/copper-bottle-hammered.jpg',
+              'https://n3.sdlcdn.com/imgs/j/j/d/NDA-Copper-Bottle-with-Glass-SDL091456758-1-52583.jpg'
+          ],
+          size: "500ml",
+          desc: "Spread the love with our Copper Love Bottle. This 500ml bottle is engraved with heartfelt messages, making it a thoughtful gift while offering the benefits of copper water.",
+          photo: "https://www.copperhealer.com/wp-content/uploads/2020/04/pure-copper-water-bottle-hammered2.jpg"
+      },
+      {
+          id: 14,
+          name: "Copper Harmony Bottle",
+          price: 1100,
+          color: "copper",
+          images: [
+              'https://www.aspkom.in/wp-content/uploads/2021/07/Plain-950ML-Set-of-2.jpg',
+              'https://assets0.mirraw.com/images/7200887/CB_136-1_zoom.jpg?1562930366',
+              'https://www.battlane.com/uploads/product-images/2019/09/27/1/large/battlane-pure-and-clean-high-grade-copper-water-bottle-1000-ml-leak-proof-copper-water-bottle-1-litre-pack-of-1-Copper-1.jpg'
+          ],
+          size: "800ml",
+          desc: "Find harmony in hydration with our Copper Harmony Bottle. This 800ml bottle features a unique design inspired by nature, bringing a sense of balance to your daily routine.",
+          photo: "https://www.copperhealer.com/wp-content/uploads/2020/04/pure-copper-water-bottle-hammered2.jpg"
+      },
+
+          {
+              id: 15,
+              name: "Copper Eco-Friendly Bottle",
+              price: 850,
+              color: "copper",
+              images: [
+                  'https://stonkraft.in/wp-content/uploads/2020/11/71V73ONmVkL._SL1500_.jpg',
+                  'https://images-na.ssl-images-amazon.com/images/I/61yMEYhE70L._AC_SL1382_.jpg',
+                  'https://cdn.shopify.com/s/files/1/0552/5107/5252/products/OrganicB-6_3_2100x2100.jpg?v=1623599394'
+              ],
+              size: "600ml",
+              desc: "Make an eco-conscious choice with our Copper Eco-Friendly Bottle. This 600ml bottle is crafted from sustainable materials and promotes the use of copper-infused water for a healthier lifestyle.",
+              photo: "https://www.copperhealer.com/wp-content/uploads/2020/04/pure-copper-water-bottle-hammered2.jpg"
+          },
+          {
+              id: 16,
+              name: "Copper Gemstone Bottle",
+              price: 1200,
+              color: "copper",
+              images: [
+                  'https://cdn.shopify.com/s/files/1/1161/0584/products/matt_copper_bottle_lid_off.jpg?v=1623944597',
+                  'https://greentree.ae/wp-content/uploads/2020/10/copper-4.png',
+                  'https://www.sharedearth-trade.co.uk/Files/130740/Img/08/COP05-4-zoom.jpg'
+              ],
+              size: "750ml",
+              desc: "Experience the fusion of elegance and wellness with our Copper Gemstone Bottle. This 750ml bottle features a gemstone-infused design, enhancing the benefits of copper water.",
+              photo: "https://www.copperhealer.com/wp-content/uploads/2020/04/pure-copper-water-bottle-hammered2.jpg"
+          },
+          {
+              id: 17,
+              name: "Copper Zen Bottle",
+              price: 950,
+              color: "copper",
+              images: [
+                  'https://n1.sdlcdn.com/imgs/i/z/e/1-L-Pure-Copper-Water-SDL844752402-1-b283d.jpg',
+                  'https://janakktp.in/wp-content/uploads/2020/08/copper-bottle-7b-scaled.jpg',
+                  'https://img.mensxp.com/media/shop/catalog/products/C/711520/copper-bottle-750-ml-251288-default.jpg'
+              ],
+              size: "500ml",
+              desc: "Find your Zen with our Copper Zen Bottle. This 500ml bottle is designed with a serene and minimalist style, helping you stay calm and centered while enjoying copper-infused water.",
+              photo: "https://www.copperhealer.com/wp-content/uploads/2020/04/pure-copper-water-bottle-hammered2.jpg"
+          },
+          {
+              id: 18,
+              name: "Copper Wellness Companion",
+              price: 750,
+              color: "copper",
+              images: [
+                  'https://www.orderyourchoice.com/118285-large_default/copper-bottle-matt-.jpg',
+                  'https://www.thehappyturtle.in/wp-content/uploads/2019/11/Copper-Bottle-1-ltr.png',
+                  'https://images-na.ssl-images-amazon.com/images/I/61HPFgiGT1L._SL1500_.jpg'
+              ],
+              size: "800ml",
+              desc: "Embrace a healthier lifestyle with our Copper Wellness Companion. This 800ml bottle is your trusted partner on the path to wellness, providing the benefits of copper water with every sip.",
+              photo: "https://www.copperhealer.com/wp-content/uploads/2020/04/pure-copper-water-bottle-hammered2.jpg"
+          },
+          {
+              id: 19,
+              name: "Copper Balance Bottle",
+              price: 1100,
+              color: "copper",
+              images: [
+                  'https://cdn.notonthehighstreet.com/fs/b8/7c/62e1-1ba1-4513-90ea-684ab2b6b106/original_pure-copper-water-bottle.jpg',
+                  'https://thevedalifestyle.com/wp-content/uploads/2020/11/B1083-1-2.jpg',
+                  'https://www.healthshots.com/wp-content/uploads/2020/09/copper-bottle.jpg'
+              ],
+              size: "1 liter",
+              desc: "Achieve balance in body and mind with our Copper Balance Bottle. This 1-liter bottle is thoughtfully designed to help you maintain harmony and well-being through copper-infused water.",
+              photo: " https://www.copperhealer.com/wp-content/uploads/2020/04/pure-copper-water-bottle-hammered2.jpg"
+          }
+      ];
+      
+      export default additionalCopperBottleData;
